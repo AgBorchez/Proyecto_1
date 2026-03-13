c41ac0ad21f3bbd4ee9f2077aaa2a04686ca4dc2
Agustín Borchez <agborchez@gmail.com>
2026-03-13 16:55:28 -0300

    cambio_apiurl

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-//export const API_SOCIOS_URL = 'https://zxgym.onrender.com/api/socios';
-export const API_SOCIOS_URL = 'https://localhost:7102/api/socios';
-//export const API_ENTRENADORES_URL = 'https://zxgym.onrender.com/api/Entrenador';
-export const API_ENTRENADORES_URL = 'https://localhost:7102/api/Entrenador';
+export const API_SOCIOS_URL = 'https://zxgym.onrender.com/api/socios';
+//export const API_SOCIOS_URL = 'https://localhost:7102/api/socios';
+export const API_ENTRENADORES_URL = 'https://zxgym.onrender.com/api/Entrenador';
+//export const API_ENTRENADORES_URL = 'https://localhost:7102/api/Entrenador';
 
 //variables globales de estado
 export const EstadoSocios = {
